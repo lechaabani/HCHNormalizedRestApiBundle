dc27fd2464376729e67fd440dd870968d5e3dca7
lechaabani <chaabani.hammadi@gmail.com>
2016-10-29 21:46:40 +0200

    Review code

--- Command/CreateErrorCodeCommand.php
+++ Command/CreateErrorCodeCommand.php
@@ -1,36 +1,47 @@
 <?php
 
-// src/AppBundle/Command/CreateUserCommand.php
+/**
+ *  Create the errors_code file
+ * 
+ * @package NormalizedRestApiBundle
+ * @subpackage Command
+ * @author Hamadi Chaabani <chaabani.hammadi@gmail.com>
+ */
 
-namespace HCH\DemoBundle\Command;
+namespace HCH\NormalizedRestApiBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface; 
+use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ *  Create the errors_code file
+ * 
+ * @package NormalizedRestApiBundle
+ * @subpackage Command
+ * @author Hamadi Chaabani <chaabani.hammadi@gmail.com>
+ */
 class CreateErrorCodeCommand extends Command {
 
   protected function configure() {
     $this
-            // the name of the command (the part after "bin/console")
-            ->setName('app:create-error-code')
+      // the name of the command (the part after "bin/console")
+      ->setName('app:create-error-code')
 
-            // the short description shown while running "php bin/console list"
-            ->setDescription('Create code error file.')
+      // the short description shown while running "php bin/console list"
+      ->setDescription('Create code error file.')
 
-            // the full command description shown when running the command with
-            // the "--help" option
-            ->setHelp("This command allows you to create code error file")
+      // the full command description shown when running the command with
+      // the "--help" option
+      ->setHelp("This command allows you to create the code error file")
     ;
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
 
-    $content = "; this is an INI file \n [parameters] \n \n ;code 500 \n 501 = An intenal error has occured"; 
+    $content = "; this is an INI file \n [parameters] \n \n ;code 500 \n 501 = An intenal error has occured";
     $fp = fopen(__DIR__ . '../../../../../app/config/errors_code.ini', "wb");
     fwrite($fp, $content);
     fclose($fp);
- 
-  }
-
+  } 
 }
